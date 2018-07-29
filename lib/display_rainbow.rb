b4e8feb461de695819e031e40a65c669fd1f6033
Andrew Persad <akpersad@gmail.com>
2018-07-29 03:37:57 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+def display_rainbow (arr)
+  # new_arr = ''
+  # arr.each do |item|
+  #   new_arr += item[0].upcase + ":"
+  #   new_arr += ' ' + item + ", "
+  # end
+  #
+  # arr_length = new_arr.length - 1
+  #
+  # puts new_arr[0, arr_length - 1]
+
+  puts "R: #{arr[0]}, O: #{arr[1]}, Y: #{arr[2]}, G: #{arr[3]}, B: #{arr[4]}, I: #{arr[5]}, V: #{arr[6]}"
+end
